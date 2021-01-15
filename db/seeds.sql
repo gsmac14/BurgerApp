e2f138cf44f925b980b186c822ad676058117a98
Girverius <gsmac1224@gmail.com>
2021-01-14 22:39:10 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (burger_name) VALUES ('Cheese burger');
+INSERT INTO burger (burger_name) VALUES ('Double cheese burger');
+INSERT INTO burger (burger_name) VALUES ('Chicken burger');
+INSERT INTO burger (burger_name) VALUES ('Veggie burger');
